a26f6f97e655c1c65108fce9689c286e6b0afe25
Hauke Petersen <hauke.petersen@fu-berlin.de>
2017-12-12 15:27:35 +0100

    compat

--- mynewtmap/include/syscfg/syscfg.h
+++ mynewtmap/include/syscfg/syscfg.h
@@ -12,4 +12,14 @@
  * sends a partial write.
  */
 #define BLE_ATT_SVR_MAX_PREP_ENTRIES        (64U)
+
+/**
+ * This is the number of multi-advertising instances. This is NOT the
+ * total number of advertising instances. The total number of
+ * advertising instances is this number plus 1 (assuming the device
+ * supports advertising).
+ */
+#define BLE_MULTI_ADV_INSTANCES             (0)
+
+
 #endif /* SYSCFG_H */
